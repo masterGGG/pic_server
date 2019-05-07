10b8064cc838dafe84fa8f5312eb0aced3680f13
masterGGG <985953408@qq.com>
2019-05-07 16:17:17 +0800

    support keeplive

---
+++ fastDFS/tracker/keep-alive.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd `dirname $0`
+appname=$1
+
+./${appname}.sh state > /dev/null 2>&1
+
+if [ $? -ne 0 ];then
+    echo "${appname} stopped abnormally, try to start it."
+    ./${appname}.sh stop
+    sleep 5
+    echo "restart ${appname} at $(date)"
+    ./${appname}.sh start
+fi
+find . -name "core*" | xargs rm -vf 
+
+exit 0
